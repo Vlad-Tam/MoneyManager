a32c93de97218556a71fde84a7a062c60fa823c6
Vlad-Tam <126288981+Vlad-Tam@users.noreply.github.com>
2023-10-24 00:14:00 +0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.moneymanager {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens com.example.moneymanager to javafx.fxml;
-    exports com.example.moneymanager;
-}
